49beaf747e5873c88acd9d0a7a6da93edfa695d4
Deb Kalyan Mohanty <debkalyanmohanty.id@gmail.com>
2023-06-06 05:31:09 +0530

    Done

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connect = require('./db');
+
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+
+
+connect();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port , ()=>console.log(`${port} Active `));
